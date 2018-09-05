d0c0f9128b944b576221b8af0322e4f0b8e97f7b
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-09-05 20:23:46 +0800

    seeded abby as teacher

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+abby = User.create(email: "arendabby2@gmail.com", first_name: "Abby", last_name: "Arend", username: "teacherAbby", password: ENV["PASSWORD"], password_confirmation: ENV["PASSWORD"])
+teacher = Teacher.new(id: 1,
+ education: "TEFL Certified, Bachelor's of Arts in Political Science",
+ years_of_experience: 4)
+teacher.user = abby
+teacher.save!
+
+
